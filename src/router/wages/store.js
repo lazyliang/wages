9794671ca08b2e3726d64b052e9761e997c81cd7
ll <1015492734@qq.com>
2018-04-20 01:04:59 +0800

    添加工资页面

--- src/router/wages/store.js
+++ src/router/wages/store.js
@@ -25,12 +25,35 @@ class WagesStore {
         this.pagination = {}
         this.list = []
         this.forms = {
-            isDelete: '',
-            state: '',
-            languageMotorcycleType: []
         }
     }
 
+    @action
+    initWages=async()=>{
+        runInAction(()=>{
+            this.loading = true
+        })
+        const res = await get(`${process.env.REACT_APP_API_URL}/wages:search`)
+        runInAction(()=>{
+            this.list = res.content.map(l=>{
+                return{
+                    id:l.id,
+                    userName:l.userName,
+                    num:l.num,
+                   year:l.year,
+                    month:l.month,
+                    sum:l.sum
+                }
+            })
+            this.pagination = {
+                total: res.totalElements,
+                results: res.size,
+                page: res.number,
+            }
+            this.forms = {}
+        })
+        this.loading = false
+    }
 
 }
 
